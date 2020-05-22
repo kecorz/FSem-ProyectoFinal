766d76af28d7c17ee045aeeead75059984eb4fb3
kecorz <kecorz1012@gmail.com>
2020-05-22 17:34:42 -0500

    primera version de modulos motor y timbre

---
+++ Modulo_Motor.ino
@@ -0,0 +1,52 @@
+// LOGICA DE FUNCIONAMIENTO DEL MOTOR
+
+//SE UTILIZARAN TRES PINES (DOS DIRECCIONES Y UN ENABLE)
+
+///////// Configuraciones GPIO para motor ///////
+int Mot1 = ;
+int Mot2 = ;
+int EnableMot = ;
+int speed = 130;
+int contadorMotor = 0;
+
+void setup(){
+
+	pinMode(Mot1, OUTPUT);
+  	pinMode(Mot2, OUTPUT);
+ 	pinMode(EnableMot, OUTPUT);
+
+}
+
+void loop(){
+
+	/// Velocidad que tendra el motor //
+ 		analogWrite(EnableMot,speed);
+ 	//digitalWrite(EnableMot, HIGH); //
+
+_
+}
+
+void handleSubmit(){
+	
+	String Motor;
+
+
+	Motor = server.arg("MOTOR"); //Obtenemos el valor de la oagina web 
+
+
+		if ( Motor == "1"){ //La puerta se abre
+		
+  		digitalWrite(motor1Pin1, LOW);
+ 		digitalWrite(motor1Pin2, HIGH); 
+  		delay(2000);
+
+	}else if(valor_boton == "0"){ //La puerta se cierra
+
+  		digitalWrite(motor1Pin1, HIGH);
+  		digitalWrite(motor1Pin2, LOW); 
+  		delay(2000);
+
+  	}
+
+
+}	
